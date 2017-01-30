78c26bbd2799082c7a062824eb5cdcbe81cf25d2
jstar <jstar@ee.pw.edu.pl>
2017-01-30 10:33:23 +0100

    Drobna refaktoryzacja: Arrays.fill zamiast pętli.

--- Gui/src/gui/DicomExplorer.java
+++ Gui/src/gui/DicomExplorer.java
@@ -15,7 +15,6 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
